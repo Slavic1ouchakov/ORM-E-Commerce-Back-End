c14dfb937abe1a7c9384cecc97fe457d9aa3dc25
Slavic1ouchakov <slavic.ouchakov@gmail.com>
2021-07-19 17:42:13 -0400

    configured category.js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,7 +7,11 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
-  },
+    category_name: {
+      type:DataTypes.STRING, 
+      allowNull: false,
+  }
+},
   {
     sequelize,
     timestamps: false,
